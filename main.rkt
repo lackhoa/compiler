ffda0dca003f36fb13ce31bad00ba762be82a711
lackhoa <vodangkhoa873@gmail.com>
2018-08-31 18:39:33 +0300

    I'm getting the feeling that this is pointless

--- main.rkt
+++ main.rkt
@@ -3,7 +3,8 @@
 
 (struct Register
   (name
-   [contents #:mutable #:auto])
+   [contents #:mutable
+             #:auto])
   #:auto-value '*unassigned*)
 
 
@@ -29,27 +30,83 @@
       (set! s null))))
 
 
+; Instructions
+(struct Instruction
+  (text
+   [execution-proc #:mutable
+                   #:auto])
+  #:auto-value null)
+
+(def (update-insts! insts labels machine)
+  (let ([pc
+         (send machine get-register 'pc)]
+        [flag
+         (send machine get-register 'flag)]
+        [stack
+         (send machine get-stack)]
+        [ops
+         (send machine get-ops)])
+    (for-each
+      (lam (inst)
+        (set-Instruction-execution-proc!
+          inst
+          (make-execution-procedure
+            (Instruction-text inst)
+            labels machine pc flag
+            stack ops)))
+      insts)))
+
+(def (make-execution-procedure inst labels machine
+                               pc flag stack ops)
+  (match (car inst)
+    ['assign
+     (make-assign inst machine
+                  labels ops pc)]
+
+    ['test
+     (make-test inst machine labels
+                ops flag pc)]
+
+    ['branch
+     (make-branch inst machine
+                  labels flag pc)]
+
+    ['goto
+     (make-goto inst machine
+                labels pc)]
+
+    ['save
+     (make-save inst machine
+                stack pc)]
+
+    ['restore
+     (make-restore inst machine
+                   stack pc)]
+
+    ['perform
+     (make-perform inst machine
+                   labels ops pc)]
+
+    [_
+     (error "Unknown instruction type" inst)]))
+
+; The machine model
 (def machine
   (class object%
     (super-new)
 
-    ; Inits
-    (init [pcI           (Register 'pc)])
-    (init [flagI         (Register 'flag)])
-    (init [stackI        (new stack%)])
-    (init [instructionsI null])
-
     ;Fields
-    (def pc           pcI)
-    (def flag         flagI)
-    (def stack        stackI)
-    (def instructions instructionsI)
+    (def pc           (Register 'pc))
+    (def flag         (Register 'flag))
+    (def stack        (new stack%))
+    (def instructions null)
 
-    ; These two fields depend on the inits.
+    ; These two fields depend on the others.
     (def the-ops
-      (list (cons 'initialize-stack
-                  (lam ()
-                    (send stack init)))))
+      (list
+        (Instruction 'initialize-stack
+                     (lam ()
+                       (send stack init)))))
 
     (def register-table
       (list pc flag))
@@ -101,9 +158,57 @@
         [(list)
          'DONE]
 
-        [ins
-         (let ([todo
-                (Instr-proc (car ins))])
-           todo)
+        [(cons next-inst _)
+         ((Instruction-execution-proc next-inst))
          ; And then we do it again
          (execute)]))))
+
+
+; The assembler
+(def (assemble controller-text machine)
+  (let-values ([(insts labels)
+                (extract-labels controller-text)])
+    (lam (insts labels)
+      (update-insts! insts
+                     labels
+                     machine)
+      insts)))
+
+(struct Label-entry
+  (name insts))
+
+(def (lookup-label labels label-name)
+  (match labels
+    [(list)
+     (error "Undefined label" label-name)]
+
+    ; Matches
+    [(cons (Label-entry (== label-name eq?)
+                        insts)
+           _)
+     insts]
+
+    ; Doesn't match
+    [(cons _ rest-labels)
+     (lookup-label rest-labels
+                   label-name)]))
+
+(def (extract-labels text)
+  (match text
+    [(list)
+     (values null null)]
+
+    [(cons next-inst
+           rest-insts)
+     (let-values ([(insts labels)
+                   (extract-labels rest-insts)])
+       (match next-inst
+         [(? symbol?)
+          (values insts
+                 (cons (Label-entry next-inst
+                                    insts)
+                       labels))]
+
+         [_
+          (values (cons (Instruction next-inst))
+                  labels)]))]))
